1add6113d8b171dbfb7f50cc89ee2571f0a6a610
greenhc502 <greenhc502@gmail.com>
2025-07-15 10:59:09 +0900

    first otd test

---
+++ src/main/java/com/otd/firstotd/FirstOtdApplication.java
@@ -0,0 +1,13 @@
+package com.otd.firstotd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstOtdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstOtdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otd/firstotd/FirstOtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otd.firstotd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstOtdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
