68178464ea6b99283922658f27b7e21ef141c9a9
d-kjh <dkjh9942@gmail.com>
2025-07-15 11:41:01 +0900

    weather

---
+++ src/main/java/com/otd/firstotd/weather/weatherController.java
@@ -0,0 +1,17 @@
+package com.otd.firstotd.weather;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/weather")
+public class weatherController {
+
+    @GetMapping
+    public String weather() {
+        return "weather";
+    }
+}
